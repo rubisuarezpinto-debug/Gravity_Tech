05ef6c88ffd9758146140b70dbd26f1582f4c40c
Warzerp <jhonthegamer2@gmail.com>
2026-04-02 15:21:17 -0500

    chore(database): add DDL structure and automatic installation scripts

---
+++ backend/database/Scripts/01-create-db.sh
@@ -0,0 +1,4 @@
+# Primer Script: Crear base de datos
+python .\01-sql-ddl-script-auto.py --sql-dir ../../ddl --user postgres --password "*" --host localhost --port 5432 --database postgres --create-script true
+# Segundo Script: Crear tablas y alterar tablas
+python .\01-sql-ddl-script-auto.py --sql-dir ../../ddl --user ecommerce_admin --password "***REMOVED***" --host localhost --port 5432 --database ecommerce_db
